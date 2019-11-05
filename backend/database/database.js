3381aaeeed750b0ea49e48b261994dd62564e99a
Pretto <maximiliano.pretto@gmail.com>
2019-11-05 20:59:55 -0300

    modificacion bbdd

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,21 +1,5 @@
-/* const Sequelize = require('sequelize');
-
-const CONNECTION_URL = 'postgres://postgres:admin@localhost:5432/libreria';
-const sequelize = new Sequelize(CONNECTION_URL);
-
-sequelize
-	.authenticate()
-	.then(() => {
-		console.log('Conectado con exito a POSGRESQL');
-	})
-	.catch(err => {
-		console.error('Error al conectarse a POSGRESQL:', err);
-	});
-
-module.exports = { sequelize, Sequelize };
-*/
-
 const { Pool } = require('pg');
+
 const pool = new Pool({
 	user: 'postgres',
 	host: 'localhost',
@@ -23,6 +7,7 @@ const pool = new Pool({
 	password: 'admin',
 	port: 5432
 });
+
 pool.query('select * from autor;', (err, res) => {
 	const autores = res.rows;
 	autores.forEach(autor => {
---
+++ scripts-sql/2_triggers.sql
@@ -0,0 +1,101 @@
+CREATE OR REPLACE FUNCTION uppercase_promocion() RETURNS trigger AS $uppercase_promocion$
+    BEGIN        
+        NEW.nombre_promocion = UPPER(NEW.nombre_promocion);
+        RETURN NEW;
+    END;
+$uppercase_promocion$ LANGUAGE plpgsql;
+
+CREATE TRIGGER uppercase_promocion_trigger BEFORE INSERT OR UPDATE ON promocion
+    FOR EACH ROW EXECUTE PROCEDURE uppercase_promocion();
+--------
+
+
+CREATE OR REPLACE FUNCTION uppercase_producto() RETURNS trigger AS $uppercase_producto$
+    BEGIN        
+        NEW.titulo = UPPER(NEW.titulo);
+        RETURN NEW;
+    END;
+$uppercase_producto$ LANGUAGE plpgsql;
+
+CREATE TRIGGER uppercase_producto_trigger BEFORE INSERT OR UPDATE ON producto
+    FOR EACH ROW EXECUTE PROCEDURE uppercase_producto();
+-------
+
+
+CREATE OR REPLACE FUNCTION uppercase_editorial() RETURNS trigger AS $uppercase_editorial$
+    BEGIN        
+        NEW.nombre_editorial = UPPER(NEW.nombre_editorial);
+        RETURN NEW;
+    END;
+$uppercase_editorial$ LANGUAGE plpgsql;
+
+CREATE TRIGGER uppercase_editorial_trigger BEFORE INSERT OR UPDATE ON editorial
+    FOR EACH ROW EXECUTE PROCEDURE uppercase_editorial();
+------------
+
+
+CREATE OR REPLACE FUNCTION uppercase_saga() RETURNS trigger AS $uppercase_saga$
+    BEGIN        
+        NEW.nombre_saga = UPPER(NEW.nombre_saga);
+        RETURN NEW;
+    END;
+$uppercase_saga$ LANGUAGE plpgsql;
+
+CREATE TRIGGER uppercase_saga_trigger BEFORE INSERT OR UPDATE ON saga
+    FOR EACH ROW EXECUTE PROCEDURE uppercase_saga();
+-------------
+
+
+CREATE OR REPLACE FUNCTION uppercase_libro() RETURNS trigger AS $uppercase_libro$
+    BEGIN        
+        NEW.idioma = UPPER(NEW.idioma);
+        NEW.edicion = UPPER(NEW.edicion);
+        NEW.descripcion = UPPER(NEW.descripcion);
+        RETURN NEW;
+    END;
+$uppercase_libro$ LANGUAGE plpgsql;
+
+CREATE TRIGGER uppercase_libro_trigger BEFORE INSERT OR UPDATE ON libro
+    FOR EACH ROW EXECUTE PROCEDURE uppercase_libro();
+-----------
+
+
+CREATE OR REPLACE FUNCTION uppercase_autor() RETURNS trigger AS $uppercase_autor$
+    BEGIN        
+        NEW.autor = UPPER(NEW.autor);
+        NEW.nacionalidad = UPPER(NEW.nacionalidad);
+        RETURN NEW;
+    END;
+$uppercase_autor$ LANGUAGE plpgsql;
+
+CREATE TRIGGER uppercase_autor_trigger BEFORE INSERT OR UPDATE ON autor
+    FOR EACH ROW EXECUTE PROCEDURE uppercase_autor();
+------------
+
+
+CREATE OR REPLACE FUNCTION uppercase_categoria() RETURNS trigger AS $uppercase_categoria$
+    BEGIN        
+        NEW.nombre_categoria = UPPER(NEW.nombre_categoria);
+        RETURN NEW;
+    END;
+$uppercase_categoria$ LANGUAGE plpgsql;
+
+CREATE TRIGGER uppercase_categoria_trigger BEFORE INSERT OR UPDATE ON categoria
+    FOR EACH ROW EXECUTE PROCEDURE uppercase_categoria();
+---------------
+
+
+CREATE OR REPLACE FUNCTION uppercase_usuario() RETURNS trigger AS $uppercase_usuario$
+    BEGIN        
+        NEW.nombre = UPPER(NEW.nombre);
+        NEW.apellido = UPPER(NEW.apellido);
+        NEW.rol = UPPER(NEW.rol);
+        RETURN NEW;
+    END;
+$uppercase_usuario$ LANGUAGE plpgsql;
+
+CREATE TRIGGER uppercase_usuario_trigger BEFORE INSERT OR UPDATE ON usuario
+    FOR EACH ROW EXECUTE PROCEDURE uppercase_usuario();
+------------------
+
+
