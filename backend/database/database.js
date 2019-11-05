c2c4ae7f9116b597b845ff3505ca65010903504b
Panchi <franciscostantini@gmail.com>
2019-11-05 20:18:41 -0300

    testeo database

--- backend/database/database.js
+++ backend/database/database.js
@@ -15,15 +15,18 @@ sequelize
 module.exports = { sequelize, Sequelize };
 */
 
-const { Pool } = require('pg')
+const { Pool } = require('pg');
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'libreria',
-  password: 'admin',
-  port: 5432,
-})
-pool.query("select * from autor;", (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
+	user: 'postgres',
+	host: 'localhost',
+	database: 'libreria',
+	password: 'admin',
+	port: 5432
+});
+pool.query('select * from autor;', (err, res) => {
+	const autores = res.rows;
+	autores.forEach(autor => {
+		console.log(`${autor.autor}`);
+	});
+	pool.end();
+});
