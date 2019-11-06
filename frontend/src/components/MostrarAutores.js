04320afa78f41408b31cbb71dcfc1b278a226e3f
Panchi <franciscostantini@gmail.com>
2019-11-06 17:12:57 -0300

    redux funcionando - action fetchAutores funcional

--- frontend/src/components/MostrarAutores.js
+++ frontend/src/components/MostrarAutores.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import { connect } from 'react-redux';
 import { fetchAutores } from '../actions/autorActions';
 
 class MostrarAutores extends Component {
-	UNSAFE_componentWillMount() {
+	componentDidMount() {
 		this.props.fetchAutores();
 	}
 
 	render() {
-		const autores = null; /*this.props.autores.map(autor => (
+		const autores = this.props.autores.map(autor => (
 			<div key={autor.id_autor}>
 				<h3>Autor: {autor.autor}</h3>
 				<p>Nacionalidad: {autor.nacionalidad}</p>
 			</div>
-		));*/
+		));
 		return (
 			<div>
 				<h1>Autores</h1>
@@ -23,12 +25,17 @@ class MostrarAutores extends Component {
 	}
 }
 
-const mapStateToProps = state => ({
+MostrarAutores.propTypes = {
+	fetchAutores: PropTypes.func.isRequired,
+	autores: PropTypes.array.isRequired
+};
+
+const mapStateToProps = state => {
 	// la palabra autores en state.autores es el nombre que pusimos en index.js en la carpeta reducers.
 	// items es el nombre que le asignamos en el reducer de autores.
-	autores: state.autores.items
-});
+	return { autores: state.autores.items };
+};
 export default connect(
-	null,
+	mapStateToProps,
 	{ fetchAutores }
 )(MostrarAutores);
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux';
 import autoresReducer from './autorReducer';
 
 export default combineReducers({
-	autor: autoresReducer
+	autores: autoresReducer
 });
